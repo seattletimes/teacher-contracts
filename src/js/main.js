9f513f851e138066e84901682bc85d5cfa47d16f
emilymeng <eng.emilym@gmail.com>
2018-08-06 23:54:37 -0700

    color updating js

--- src/js/main.js
+++ src/js/main.js
@@ -7,47 +7,44 @@ var element = document.querySelector("leaflet-map");
 var L = element.leaflet;
 var map = element.map;
 
-//ICH code for popup template if needed----------
-var ich = require("icanhaz");
-var templateFile = require("./_popup.html");
-ich.addTemplate("popup", templateFile);
-
 var xhr = require("./lib/xhr");
 var template = require("./lib/dot").compile(require("./_tooltip.html"));
 
-var change = window.change;
+var allData = window.allData;
 var featureLookup = {};
-
-var scale = [
-  { limit: .2, color: "#FFE353" },
-  { limit: .4, color: "#EDB646" },
-  { limit: .6, color: "#D38C40" },
-  { limit: .8, color: "#B1673B" },
-  { limit: 1.0, color: "#8A4734" }
-];
 
 xhr("./assets/schools.geo.json", function(err, data) {
 	var fillKey = function() {
 	};
 	fillKey();
-	var paintChange = function(feature) {
-    // var GEOID = [feature.properties.GEOID];
-    // var fillColor = "transparent";
-    // var max = getMaxSold();
-};
+	
+  var findDistrictData = function(geoid) {
+    var districtData = allData.find(function(individualChange) {
+      return individualChange.GEOID == geoid;
+    });
+
+    return districtData;
+  }
   
   var paint = function(feature) {
-    var GEOID = [feature.properties.GEOID];
-    var fillColor = scale[change];
-    if (GEOID && typeof GEOID.value == "number") {
-      for (var i = 0; i < scale.length; i++) {
-        var pigment = scale[i];
-        if (GEOID.value <= pigment.limit) {
-          fillColor = pigment.color;
-          break;
-        }
+    var fillColor = "#00GG00";
+
+    // GEOID == identifier for the disrict
+    var districtData = findDistrictData(feature.properties.GEOID);
+
+    if (districtData) {
+      // District data will either be a number or undefined - if a number use it to color
+      var min = Number.parseFloat(districtData.pChangeMin);
+
+      if (!isNaN(min)) {
+        fillColor = min <= 0 ? '#FFE353' :
+                    min <= .07 ? '#D38C40' :
+                    min <= .08 ? '#B1673B' :
+                    min <= .1 ? '#EDB646' :
+                    '#8A4734';
       }
     }
+
     return {
       fillColor,
       weight: 1,
@@ -55,21 +52,18 @@ xhr("./assets/schools.geo.json", function(err, data) {
       fillOpacity: .7
     }
   };
+
 	var onClick = function(e) {
-		map.openPopup(template(data), e.latlng)
+    var districtData = findDistrictData(this.GEOID);
+		map.openPopup(template(districtData), e.latlng)
   };
 
-  console.log(change);
 	var schools = L.geoJson(data, {
     style: paint,
     onEachFeature: function(feature, layer) {
       layer.on("click", onClick.bind(feature.properties));
     }
   });
-
-	var onEachFeature = function(feature, layer) {
-  	layer.bindPopup(ich.popup(feature.properties))
-	};
 
   schools.addTo(map);
   map.fitBounds(schools.getBounds());
